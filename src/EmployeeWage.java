92f5f4e34d8e832027bb1c0166de0d0c55bf9c7d
amtksms97 <amtksms97@gmail.com>
2020-11-11 17:38:52 +0530

    Added: "Code to find attendance of the employee"

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,8 +1,19 @@
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation Program");
-
+		Random rnum = new Random();
+		int random = rnum.nextInt(2);
+		if(random==0)
+		{
+			System.out.println("Absent");
+		}
+		else
+		{
+			System.out.println("Present");
+		}
 	}
 
 }
