7f7ee2747449264b3015a2585fa976413a1c3ec8
amtksms97 <amtksms97@gmail.com>
2020-11-11 17:58:29 +0530

    First Commit

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,16 +4,5 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation Program");
-		Random rnum = new Random();
-		int random = rnum.nextInt(2);
-		if(random==0)
-		{
-			System.out.println("Absent");
-		}
-		else
-		{
-			System.out.println("Present");
-		}
 	}
-
 }
